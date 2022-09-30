d2749a1f996bb397de25be0db71806863c2d2399
ryourst <45187719+ryourst@users.noreply.github.com>
2022-09-30 13:36:24 -0700

    add argocd cli

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-echo If difficulty is encountered, please let us know in the academy channel on Calico Users Slack.
-
 [[ "${AWS_EXECUTION_ENV}" != "CloudShell" ]] && { echo "Please use AWS CloudShell."; exit 1; }
 
 [[ ! -d "`echo ~`/.local/bin" ]] && mkdir -p "`echo ~`/.local/bin"
@@ -11,12 +9,14 @@ echo Downloading lab assets.
 # calicoctl -h &> /dev/null || (echo "Downloading calicoctl"; curl -Lo "`echo ~`/.local/bin/calicoctl" https://github.com/projectcalico/calicoctl/releases/download/v3.17.1/calicoctl-linux-amd64 )
 kubectl -h &> /dev/null || (echo "Downloading kubectl"; curl -Lo "`echo ~`/.local/bin/kubectl" https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl )
 eksctl -h &> /dev/null || (echo "Downloading eksctl"; curl -Lo /tmp/eksctl.tgz "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_arm64.tar.gz" && tar xzvf /tmp/eksctl.tgz > /dev/null && mv eksctl ~/.local/bin/eksctl )
+argocd -h &> /dev/null || (echo "Downloading argocd"; curl -Lo "`echo ~`/.local/bin/argocd" https://github.com/argoproj/argo-cd/releases/download/v2.4.12/argocd-linux-amd64 )
 chmod +x "`echo ~`/.local/bin/"*
 
 echo Verifying installation.
 # calicoctl -h &> /dev/null || { echo "Error downloading calicoctl. Please try again."; rm -f "`echo ~`/.local/bin/calicoctl"; exit 1; }
 kubectl -h &> /dev/null || { echo "Error downloading kubectl. Please try again."; rm -f "`echo ~`/.local/bin/kubectl"; exit 1; }
 eksctl -h &> /dev/null || { echo "Error downloading eksctl. Please try again."; rm -f "`echo ~`/.local/bin/eksctl"; exit 1; }
+argocd -h &> /dev/null || { echo "Error downloading argocd. Please try again."; rm -f "`echo ~`/.local/bin/argocd"; exit 1; }
 echo Installation complete.
 
 exit 0
