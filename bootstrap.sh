ad63e7e06cf4b68e4ba617e9455f3f08b14e2b08
ryourst <ryourstone@yahoo.com>
2022-09-30 21:24:46 +0000

    added istioctl

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,9 @@
 echo Downloading lab assets.
 # calicoctl -h &> /dev/null || (echo "Downloading calicoctl"; curl -Lo "`echo ~`/.local/bin/calicoctl" https://github.com/projectcalico/calicoctl/releases/download/v3.17.1/calicoctl-linux-amd64 )
 kubectl -h &> /dev/null || (echo "Downloading kubectl"; curl -Lo "`echo ~`/.local/bin/kubectl" https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl )
-eksctl -h &> /dev/null || (echo "Downloading eksctl"; curl -Lo /tmp/eksctl.tgz "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_arm64.tar.gz" && tar xzvf /tmp/eksctl.tgz > /dev/null && mv eksctl ~/.local/bin/eksctl )
-argocd -h &> /dev/null || (echo "Downloading argocd"; curl -Lo "`echo ~`/.local/bin/argocd" https://github.com/argoproj/argo-cd/releases/download/v2.4.12/argocd-linux-amd64 )
+eksctl -h &> /dev/null || (echo "Downloading eksctl"; curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp > /dev/null && mv /tmp/eksctl ~/.local/bin/eksctl )
+istioctl -h &> /dev/null || (echo "Downloading istioctl"; cd ~ && curl --silent -L https://istio.io/downloadIstio | ISTIO_VERSION=1.15.1 TARGET_ARCH=x86_64 sh - && mv "`echo ~`"/istio-?.*.*/bin/istioctl ~/.local/bin/istioctl )
+argocd -h &> /dev/null || (echo "Downloading argocd"; curl --silent -Lo "`echo ~`/.local/bin/argocd" https://github.com/argoproj/argo-cd/releases/download/v2.4.12/argocd-linux-amd64 )
 chmod +x "`echo ~`/.local/bin/"*
 
 echo Verifying installation.
@@ -20,3 +21,5 @@ argocd -h &> /dev/null || { echo "Error downloading argocd. Please try again."; 
 echo Installation complete.
 
 exit 0
+
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.15.1 TARGET_ARCH=x86_64 sh -
