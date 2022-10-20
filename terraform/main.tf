a312648b22437f98bca48896dc9d50884c49f0a7
RY <ryourstone@yahoo.com>
2022-10-20 21:54:03 +0000

    added ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,4 +73,20 @@ resource "aws_key_pair" "lab_key" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+resource "aws_instance" "k8s_node" {
+  instance_type          = "t3.medium"
+  ami                    = data.aws_ami.ubuntu_node.id
+  key_name               = aws_key_pair.lab_key.id
+  vpc_security_group_ids = [aws_security_group.lab_sg.id]
+  subnet_id              = aws_subnet.lab_net.id
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    name = "lab"
+  }
+}
+
 
