cce948374202aae1d16025bedf273c93db1ff4ff
ryourst <45187719+ryourst@users.noreply.github.com>
2022-10-23 13:50:24 -0700

    Update 3-servers.tf

--- terraform/3-servers.tf
+++ terraform/3-servers.tf
@@ -3,6 +3,22 @@ resource "aws_key_pair" "lab_key" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+resource "aws_instance" "control" {
+  instance_type          = "t2.small"
+  ami                    = data.aws_ami.ubuntu_node.id
+  key_name               = aws_key_pair.lab_key.id
+  vpc_security_group_ids = [aws_security_group.lab_sg.id]
+  subnet_id              = aws_subnet.lab_net.id
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    name = "lab"
+  }
+}
+
 resource "aws_instance" "k8s-master" {
   instance_type          = "t3.medium"
   ami                    = data.aws_ami.ubuntu_node.id
