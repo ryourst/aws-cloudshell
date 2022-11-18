9b15578a84ac4a398866f38d749526d6deb2afbe
ryourst <45187719+ryourst@users.noreply.github.com>
2022-11-18 11:24:50 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,11 @@ echo Downloading lab assets.
 # calicoctl -h &> /dev/null || (echo "Downloading calicoctl"; curl -Lo "`echo ~`/.local/bin/calicoctl" https://github.com/projectcalico/calicoctl/releases/download/v3.17.1/calicoctl-linux-amd64 )
 kubectl -h &> /dev/null || (echo "Downloading kubectl"; curl -Lo "`echo ~`/.local/bin/kubectl" https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl )
 eksctl -h &> /dev/null || (echo "Downloading eksctl"; curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp > /dev/null && mv /tmp/eksctl ~/.local/bin/eksctl )
-istioctl -h &> /dev/null || (echo "Downloading istioctl"; cd ~ && curl --silent -L https://istio.io/downloadIstio | ISTIO_VERSION=1.15.1 TARGET_ARCH=x86_64 sh - > /dev/null && mv "`echo ~`"/istio-?.*.*/bin/istioctl ~/.local/bin/istioctl )
+istioctl -h &> /dev/null || (echo "Downloading istioctl"; cd ~ && curl --silent -L https://istio.io/downloadIstio | ISTIO_VERSION=1.16.0 TARGET_ARCH=x86_64 sh - > /dev/null && mv "`echo ~`"/istio-?.*.*/bin/istioctl ~/.local/bin/istioctl )
 argocd -h &> /dev/null || (echo "Downloading argocd"; curl --silent -Lo "`echo ~`/.local/bin/argocd" https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64 )
-helm -h &> /dev/null || (echo "Downloading helm"; curl --silent --location "https://get.helm.sh/helm-v3.10.0-linux-amd64.tar.gz" | tar xz -C /tmp > /dev/null && mv /tmp/linux-amd64/helm ~/.local/bin/helm )
-terraform -h &> /dev/null || (echo "Downloading terraform"; curl --silent --location "https://releases.hashicorp.com/terraform/1.3.3/terraform_1.3.3_linux_amd64.zip" | zcat > ~/.local/bin/terraform )
-cilium -h &> /dev/null || (echo "Downloading cilium"; curl --silent --location "https://github.com/cilium/cilium-cli/releases/download/v0.12.4/cilium-linux-amd64.tar.gz" | tar xz -C ~/.local/bin/)
+helm -h &> /dev/null || (echo "Downloading helm"; curl --silent --location "https://get.helm.sh/helm-v3.10.2-linux-amd64.tar.gz" | tar xz -C /tmp > /dev/null && mv /tmp/linux-amd64/helm ~/.local/bin/helm )
+terraform -h &> /dev/null || (echo "Downloading terraform"; curl --silent --location "https://releases.hashicorp.com/terraform/1.3.5/terraform_1.3.5_linux_amd64.zip" | zcat > ~/.local/bin/terraform )
+cilium -h &> /dev/null || (echo "Downloading cilium"; curl --silent --location "https://github.com/cilium/cilium-cli/releases/download/v0.12.7/cilium-linux-amd64.tar.gz" | tar xz -C ~/.local/bin/)
 chmod +x "`echo ~`/.local/bin/"*
 
 echo Verifying installation.
